606d7e2e19655a8acc4e263ea18c86c8616648b4
Kikit <it1458@uom.edu.gr>
2017-09-02 11:53:17 +0300

    New stuff implemented. Conflicts solved!

--- app/src/main/java/com/example/android/happytravellair/FlightFragment.java
+++ app/src/main/java/com/example/android/happytravellair/FlightFragment.java
@@ -1,5 +1,6 @@
 package com.example.android.happytravellair;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
@@ -12,8 +13,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -75,12 +78,6 @@ public class FlightFragment extends Fragment {
                 "B to A - 20/02/45",
                 "A to B - 25/02/45",
                 "A to D - 26/02/45",
-                "___",
-                "A to B - 23/02/45",
-                "C to B - 23/02/45",
-                "B to C - 23/02/45",
-                "A to D - 23/02/45",
-                "D to B - 23/02/45",
         };
         List<String> flights = new ArrayList<String>(Arrays.asList(data));
         mFlightsAdapter = new ArrayAdapter<String>(
@@ -91,6 +88,15 @@ public class FlightFragment extends Fragment {
 
         ListView listview = (ListView) rootView.findViewById(R.id.listview_flights);
         listview.setAdapter(mFlightsAdapter);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String strItem = mFlightsAdapter.getItem(position);
+                Intent intent = new Intent(getActivity(), DetailActivity.class)
+                        .putExtra(Intent.EXTRA_TEXT, strItem);
+                startActivity(intent);
+            }
+        });
 
         return rootView;
     }
@@ -135,19 +141,14 @@ public class FlightFragment extends Fragment {
                         .appendQueryParameter(UNITS_PARAM, units)
                         .appendQueryParameter(DAYS_PARAM, Integer.toString(numOfDays))
                         .appendQueryParameter(APPID_PARAM,"?").build();
-                        /*.appendQueryParameter(FORMAT_PARAM, format)
-                        .appendQueryParameter(UNITS_PARAM, units)
-                        .appendQueryParameter(DAYS_PARAM, Integer.toString(numOfDays)*/
-
 
                 URL url = new URL(builtUri.toString());
-
-                Log.v(LOG_TAG, "Built URI: " + builtUri.toString());
+                Log.e(LOG_TAG, "Built URI: " + builtUri.toString());
 
                 // Create the request to OpenWeatherMap, and open the connection
                 urlConnection = (HttpURLConnection) url.openConnection();
                 urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
+                // urlConnection.connect(); --> getInputStream() opens the connection anyway
 
                 // Read the input stream into a String
                 InputStream inputStream = urlConnection.getInputStream();
@@ -171,9 +172,10 @@ public class FlightFragment extends Fragment {
                     return null;
                 }
                 flightJsonStr = buffer.toString();
-                Log.v(LOG_TAG, "Json String: " + flightJsonStr);
+                Log.e(LOG_TAG, "Json String: " + flightJsonStr);
             } catch (IOException e) {
                 Log.e("PlaceholderFragment", "Error ", e);
+                e.printStackTrace();
                 // If the code didn't successfully get the weather data, there's no point in attemping
                 // to parse it.
                 return null;
@@ -186,27 +188,126 @@ public class FlightFragment extends Fragment {
                         reader.close();
                     } catch (final IOException e) {
                         Log.e("PlaceholderFragment", "Error closing stream", e);
+                        e.printStackTrace();
                     }
                 }
             }
 
-            /*try{
+            try{
                 return getWeatherDataFromJson(flightJsonStr, numOfDays);
             }catch (JSONException e) {
                 Log.e(LOG_TAG, e.getMessage(), e);
                 e.printStackTrace();
-            }*/
+            }
             return null;
         }
 
-        /*@Override
+        /* The date/time conversion code is going to be moved outside the asynctask later,
+         * so for convenience we're breaking it out into its own method now.
+         */
+        private String getReadableDateString(long time){
+            // Because the API returns a unix timestamp (measured in seconds),
+            // it must be converted to milliseconds in order to be converted to valid date.
+            SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("EEE MMM dd");
+            return shortenedDateFormat.format(time);
+        }
+
+        /**
+         * Prepare the weather high/lows for presentation.
+         */
+        private String formatHighLows(double high, double low) {
+            // For presentation, assume the user doesn't care about tenths of a degree.
+            long roundedHigh = Math.round(high);
+            long roundedLow = Math.round(low);
+
+            String highLowStr = roundedHigh + "/" + roundedLow;
+            return highLowStr;
+        }
+
+        /**
+         * Take the String representing the complete forecast in JSON Format and
+         * pull out the data we need to construct the Strings needed for the wireframes.
+         *
+         * Fortunately parsing is easy:  constructor takes the JSON string and converts it
+         * into an Object hierarchy for us.
+         */
+        private String[] getWeatherDataFromJson(String forecastJsonStr, int numDays)
+                throws JSONException {
+
+            // These are the names of the JSON objects that need to be extracted.
+            final String OWM_LIST = "list";
+            final String OWM_WEATHER = "weather";
+            final String OWM_TEMPERATURE = "temp";
+            final String OWM_MAX = "max";
+            final String OWM_MIN = "min";
+            final String OWM_DESCRIPTION = "main";
+
+            JSONObject forecastJson = new JSONObject(forecastJsonStr);
+            JSONArray weatherArray = forecastJson.getJSONArray(OWM_LIST);
+
+            // OWM returns daily forecasts based upon the local time of the city that is being
+            // asked for, which means that we need to know the GMT offset to translate this data
+            // properly.
+
+            // Since this data is also sent in-order and the first day is always the
+            // current day, we're going to take advantage of that to get a nice
+            // normalized UTC date for all of our weather.
+
+            Time dayTime = new Time();
+            dayTime.setToNow();
+
+            // we start at the day returned by local time. Otherwise this is a mess.
+            int julianStartDay = Time.getJulianDay(System.currentTimeMillis(), dayTime.gmtoff);
+
+            // now we work exclusively in UTC
+            dayTime = new Time();
+
+            String[] resultStrs = new String[numDays];
+            for(int i = 0; i < weatherArray.length(); i++) {
+                // For now, using the format "Day, description, hi/low"
+                String day;
+                String description;
+                String highAndLow;
+
+                // Get the JSON object representing the day
+                JSONObject dayForecast = weatherArray.getJSONObject(i);
+
+                // The date/time is returned as a long.  We need to convert that
+                // into something human-readable, since most people won't read "1400356800" as
+                // "this saturday".
+                long dateTime;
+                // Cheating to convert this to UTC time, which is what we want anyhow
+                dateTime = dayTime.setJulianDay(julianStartDay+i);
+                day = getReadableDateString(dateTime);
+
+                // description is in a child array called "weather", which is 1 element long.
+                JSONObject weatherObject = dayForecast.getJSONArray(OWM_WEATHER).getJSONObject(0);
+                description = weatherObject.getString(OWM_DESCRIPTION);
+
+                // Temperatures are in a child object called "temp".  Try not to name variables
+                // "temp" when working with temperature.  It confuses everybody.
+                JSONObject temperatureObject = dayForecast.getJSONObject(OWM_TEMPERATURE);
+                double high = temperatureObject.getDouble(OWM_MAX);
+                double low = temperatureObject.getDouble(OWM_MIN);
+
+                highAndLow = formatHighLows(high, low);
+                resultStrs[i] = day + " - " + description + " - " + highAndLow;
+            }
+
+            for (String s : resultStrs) {
+                Log.v(LOG_TAG, "Forecast entry: " + s);
+            }
+            return resultStrs;
+        }
+
+        @Override
         protected void onPostExecute(String[] result) {
             if(result != null) {
                 mFlightsAdapter.clear();
-                for(String s: result){
-                    mFlightsAdapter.add(s);
+                for(String flight : result){
+                    mFlightsAdapter.add(flight);
                 }
             }
-        }*/
+        }
     }
 }
